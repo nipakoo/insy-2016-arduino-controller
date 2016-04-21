807e20f72984206607f6a033a9f42bcd21d61a82
Ville Vainio <vivainio@cs.helsinki.fi>
2016-04-21 13:33:16 +0300

    Add logic for sending/receiving a package

--- server.js
+++ server.js
@@ -6,7 +6,60 @@ var rootRef = new Firebase('https://insy-2016-web.firebaseio.com/');
 rootRef.authWithPassword({
   email: 'locker_system@example.com',
   password: 'insy2016'
+}, function (error, authData) {
+  if (error) {
+    console.log('Login Failed!', error);
+  } else {
+    console.log('Authenticated successfully!');
+    setupFirebaseListeners();
+  }
 });
+
+var setupFirebaseListeners = function () {
+  this.updatePackage = function (snapshot) {
+    var ref = snapshot.ref();
+    var data = snapshot.val();
+
+    package.updateStatus(ref, data);
+  };
+
+  rootRef.child('packages').on('child_added', this.updatePackage);
+  rootRef.child('packages').on('child_changed', this.updatePackage);
+};
+
+var package = (function () {
+  this.updateStatus = function (ref, data) {
+    if (data.is_sending) {
+      this.setReadyForPickup(ref);
+    }
+
+    if (data.is_receiving) {
+      this.setPackageAsDelivered(ref);
+    }
+  }.bind(this);
+
+  this.setReadyForPickup = function (ref) {
+    console.log('Marking package ready for pickup');
+    ref.update({
+      is_delivered: false,
+      is_sending: false,
+      updated_at: Firebase.ServerValue.TIMESTAMP
+    });
+  };
+
+  this.setPackageAsDelivered = function (ref) {
+    console.log('Marking package as delivered');
+    ref.update({
+      is_delivered: true,
+      is_receiving: false,
+      updated_at: Firebase.ServerValue.TIMESTAMP
+    });
+  };
+
+  return {
+    updateStatus: this.updateStatus
+  };
+})();
 
 var button_pin = process.argv[2];
 var servo_pin = process.argv[3];
@@ -14,17 +67,16 @@ var servo_pin = process.argv[3];
 var button;
 var servo;
 
-var changed_package;
+var board = new five.Board();
 
-var board = new five.Board();  
-board.on("ready", function() {  
+board.on('ready', function() {
   console.log('Arduino connected');
 
-  button = new Button(button_pin)
+  button = new Button(button_pin);
   servo = new Servo(servo_pin);
 
   button.on('hold', function (data) {
-    console.log("Door closed");
+    console.log('Door closed');
 
     servo.min();
     if (changed_package.is_sending) {
@@ -36,20 +88,7 @@ board.on("ready", function() {
         is_receiving: false
       });
     }
-
   });
 });
 
-rootRef.child("packages").on("child_changed", function (snapshot) {
-  console.log("received");
-  changed_package = snapshot.val();
-
-  if (changed_package.is_sending) {
-    servo.max();
-  } if (changed_package.is_receiving) {
-    servo.max();
-  }
-
-});
-
-console.log('Waiting for connection'); 
+console.log('Waiting for connection');
